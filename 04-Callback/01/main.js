398e009af90e009ec04a60ee1ec22e3c3e6339f9
NarawitC <narawit.developer@outlook.com>
2022-03-28 13:37:53 +0700

    Finish 2

--- 04-Callback/01/main.js
+++ 04-Callback/01/main.js
@@ -1,16 +1,16 @@
 function checkPermission(role, yes, no) {
-  yes = function ACCESSGRANTED() {
-    alert('ACCESS GRANTED');
-  };
-  no = function ACCESSDENIED() {
-    alert('ACCESS DENIED');
-  };
-
   if (role == 'ADMIN') {
-    return yes();
+    yes();
   } else {
-    return no();
+    no();
   }
 }
 
-// checkPermission('admin');
+const yes1 = function ACCESSGRANTED() {
+  alert('ACCESS GRANTED');
+};
+const no1 = function ACCESSDENIED() {
+  alert('ACCESS DENIED');
+};
+
+checkPermission('ADMIN', yes1, no1);
